2516a8cf112ed7e123e931275e8c17302f91c266
LeeJinyoung218 <amy021899@gmail.com>
2024-03-10 00:37:47 +0900

    :ambulance: /countries api가 엔티티 대신 dto를 반환하도록 수정

--- src/main/java/kakao/school/what/web/CountryController.java
+++ src/main/java/kakao/school/what/web/CountryController.java
@@ -2,6 +2,7 @@ package kakao.school.what.web;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kakao.school.what.domain.Country;
+import kakao.school.what.dto.CountryResponseDto;
 import kakao.school.what.service.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +20,7 @@ public class CountryController {
 
     @GetMapping("/countries")
     @ResponseBody
-    public List<Country> getCountryList() {
-        return countryService.listCountry();
+    public List<CountryResponseDto> getCountryList() {
+        return countryService.listCountryDto();
     }
 }
