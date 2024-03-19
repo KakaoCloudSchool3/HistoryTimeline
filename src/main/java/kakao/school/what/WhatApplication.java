2ed61ebe2d61993567a9f4d431361378843f5071
LeeJinyoung218 <amy021899@gmail.com>
2024-03-19 16:49:37 +0900

    :bug: @EnableJpaAuditing 어노테이션 추가 - 자동으로 날짜 시간 안들어가는 문제 해결

--- src/main/java/kakao/school/what/WhatApplication.java
+++ src/main/java/kakao/school/what/WhatApplication.java
@@ -2,6 +2,7 @@ package kakao.school.what;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableJpaAuditing
 public class WhatApplication {
     @RequestMapping("/")
     String home(){
