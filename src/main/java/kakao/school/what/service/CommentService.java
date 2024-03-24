61e95dc613ea39d6abc5a12970f8b7b263e8d50f
Sang Min Park <minisstar@naver.com>
2024-03-24 21:52:56 +0900

    ✨ 제목,유저아이디로 저장 백엔드구현

--- src/main/java/kakao/school/what/service/CommentService.java
+++ src/main/java/kakao/school/what/service/CommentService.java
@@ -22,4 +22,16 @@ public class CommentService {
                 .map(content -> commentRepository.findByContent_ContentId(content.getContentId()))
                 .orElse(Collections.emptyList());
     }
+
+    // 유저 ID를 포함한 새 댓글 저장 메소드
+    public Comment saveComment(String title, String note, Long userId) {
+        return contentRepository.findByTitle(title)
+                .map(content -> {
+                    Comment newComment = new Comment();
+                    newComment.setContent(content); // 찾은 Content 객체를 설정
+                    newComment.setNote(note); // 댓글 내용 설정
+                    newComment.setUserId(userId); // 유저 ID 설정
+                    return commentRepository.save(newComment); // Comment 저장
+                }).orElse(null); // 해당 제목의 Content가 없을 경우 null 반환
+    }
 }
