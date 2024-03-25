889e5ff90e471fae790fda35e1af208bdbd9e0ea
Sang Min Park <minisstar@naver.com>
2024-03-25 11:55:26 +0900

    ✨ 제목을 요청시 추천수반환 백엔드구현

--- src/main/java/kakao/school/what/service/UserLikeService.java
+++ src/main/java/kakao/school/what/service/UserLikeService.java
@@ -4,6 +4,8 @@ import kakao.school.what.repository.UserLikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kakao.school.what.repository.ContentRepository;
+
 @Service
 public class UserLikeService {
 
@@ -14,7 +16,14 @@ public class UserLikeService {
         this.userLikeRepository = userLikeRepository;
     }
 
-    public Long getContentLikesCount(Long contentId) {
-        return userLikeRepository.countByContentId(contentId);
+    @Autowired
+    private ContentRepository contentRepository;
+
+    public Long getContentLikesCountByTitle(String title) {
+        return contentRepository.findByTitle(title)
+                .map(content -> userLikeRepository.countByContentId(content.getContentId()))
+                .orElse(0L); // 제목에 해당하는 컨텐츠가 없으면 좋아요 개수를 0으로 반환
     }
+
+
 }
