e929ccdeb4345f8a7acbf2f7de58ca1a94316dd0
Sang Min Park <minisstar@naver.com>
2024-03-25 13:31:22 +0900

    ♻️ contentid로 get받는걸 제목으로 받는방식으로 수정

--- src/main/java/kakao/school/what/service/UserDislikeService.java
+++ src/main/java/kakao/school/what/service/UserDislikeService.java
@@ -3,18 +3,23 @@ package kakao.school.what.service;
 import kakao.school.what.repository.UserDislikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import kakao.school.what.repository.ContentRepository;
 
 @Service
 public class UserDislikeService {
 
     private final UserDislikeRepository userDislikeRepository;
+    private final ContentRepository contentRepository; // ContentRepository 의존성 주입
 
     @Autowired
-    public UserDislikeService(UserDislikeRepository userDislikeRepository) {
+    public UserDislikeService(UserDislikeRepository userDislikeRepository, ContentRepository contentRepository) {
         this.userDislikeRepository = userDislikeRepository;
+        this.contentRepository = contentRepository; // 생성자를 통해 주입
     }
 
-    public Long getContentDislikesCount(Long contentId) {
-        return userDislikeRepository.countByContentId(contentId);
+    public Long getContentDislikesCountByTitle(String title) {
+        return contentRepository.findByTitle(title)
+                .map(content -> userDislikeRepository.countByContentId(content.getContentId()))
+                .orElse(0L); // 제목에 해당하는 컨텐츠가 없으면 안좋아요 개수를 0으로 반환
     }
 }
--- src/main/java/kakao/school/what/web/UserDislikeController.java
+++ src/main/java/kakao/school/what/web/UserDislikeController.java
@@ -3,7 +3,7 @@ package kakao.school.what.web;
 import kakao.school.what.service.UserDislikeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,9 +16,8 @@ public class UserDislikeController {
         this.userDislikeService = userDislikeService;
     }
 
-    // 특정 contentId에 대한 비추천 개수를 조회하는 엔드포인트
-    @GetMapping("/dislikes/count/{contentId}")
-    public Long getContentDislikesCount(@PathVariable Long contentId) {
-        return userDislikeService.getContentDislikesCount(contentId);
+    @GetMapping("/dislikes/count-by-title")
+    public Long getContentDislikesCountByTitle(@RequestParam String title) {
+        return userDislikeService.getContentDislikesCountByTitle(title);
     }
 }
