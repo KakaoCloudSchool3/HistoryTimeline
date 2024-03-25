a763bd514c83f398bd81ea5716f461a1d7c33104
Sang Min Park <minisstar@naver.com>
2024-03-25 14:08:07 +0900

    ✨ 제목,유저id를 받아서 저장하기 구현

--- src/main/java/kakao/school/what/service/UserDislikeService.java
+++ src/main/java/kakao/school/what/service/UserDislikeService.java
@@ -1,9 +1,14 @@
 package kakao.school.what.service;
 
+import kakao.school.what.domain.Content;
+import kakao.school.what.domain.UserDislike;
 import kakao.school.what.repository.UserDislikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import kakao.school.what.repository.ContentRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class UserDislikeService {
@@ -21,5 +26,16 @@ public class UserDislikeService {
         return contentRepository.findByTitle(title)
                 .map(content -> userDislikeRepository.countByContentId(content.getContentId()))
                 .orElse(0L); // 제목에 해당하는 컨텐츠가 없으면 안좋아요 개수를 0으로 반환
+    }
+
+    public void addDislikeByTitleAndUserId(String title, Long userId) {
+        Optional<Content> contentOptional = contentRepository.findByTitle(title);
+        contentOptional.ifPresent(content -> {
+            UserDislike userDislike = new UserDislike();
+            userDislike.setContentId(content.getContentId());
+            userDislike.setUserId(userId);
+            userDislike.setCreatedAt(LocalDateTime.now());
+            userDislikeRepository.save(userDislike);
+        });
     }
 }
--- src/main/java/kakao/school/what/web/UserDislikeController.java
+++ src/main/java/kakao/school/what/web/UserDislikeController.java
@@ -2,9 +2,8 @@ package kakao.school.what.web;
 
 import kakao.school.what.service.UserDislikeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserDislikeController {
@@ -19,5 +18,11 @@ public class UserDislikeController {
     @GetMapping("/dislikes/count-by-title")
     public Long getContentDislikesCountByTitle(@RequestParam String title) {
         return userDislikeService.getContentDislikesCountByTitle(title);
+    }
+
+    @PostMapping("/dislikes/add")
+    public ResponseEntity<?> addDislikeByTitle(@RequestParam String title, @RequestParam Long userId) {
+        userDislikeService.addDislikeByTitleAndUserId(title, userId);
+        return ResponseEntity.ok().build();
     }
 }
