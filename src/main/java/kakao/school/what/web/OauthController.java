2ee9020f024ac13031a8611987005a8f4b68f9a9
LeeJinyoung218 <amy021899@gmail.com>
2024-03-26 16:56:43 +0900

    :bug: 로그인 api Service, Param, Response 수정

--- src/main/java/kakao/school/what/web/OauthController.java
+++ src/main/java/kakao/school/what/web/OauthController.java
@@ -5,11 +5,13 @@ import kakao.school.what.service.OAuthLoginService;
 import kakao.school.what.util.KakaoLoginParams;
 import kakao.school.what.util.NaverLoginParams;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.result.view.RedirectView;
+
+import java.util.NavigableSet;
 
 @RestController
 @RequestMapping("/app/login")
@@ -17,13 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class OauthController {
     private final OAuthLoginService oAuthLoginService;
 
-    @PostMapping("/kakao")
-    public ResponseEntity<OAuthInfoResponse> loginKakao(@RequestBody KakaoLoginParams params) {
+    @GetMapping("/kakao")
+    public ResponseEntity<OAuthInfoResponse> loginKakao(@RequestParam KakaoLoginParams params) {
+        System.out.println("Kakao");
         return ResponseEntity.ok(oAuthLoginService.login(params));
     }
 
-    @PostMapping("/naver")
-    public ResponseEntity<OAuthInfoResponse> loginNaver(@RequestBody NaverLoginParams params) {
-        return ResponseEntity.ok(oAuthLoginService.login(params));
+    @GetMapping("/naver")
+    public ResponseEntity<String> loginNaver(@RequestParam("code") String code,
+                                                        @RequestParam("code") String state) {
+        NaverLoginParams params = new NaverLoginParams();
+        params.setCode(code);
+        params.setState(state);
+        oAuthLoginService.login(params);
+
+        String externalUrl = "http://localhost:3000/map";
+
+        return ResponseEntity.status(HttpStatus.FOUND)
+                .header("Location", externalUrl)
+                .body("Redirecting to external URL: " + externalUrl);
     }
 }
