2aa9098ad42d0d8c8d242d79015390d9e74f8945
LeeJinyoung218 <amy021899@gmail.com>
2024-03-27 09:29:57 +0900

    :poop: 쿠키 세션 설정 중

--- src/main/java/kakao/school/what/web/OauthController.java
+++ src/main/java/kakao/school/what/web/OauthController.java
@@ -1,11 +1,12 @@
 package kakao.school.what.web;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import kakao.school.what.dto.oauth.OAuthInfoResponse;
 import kakao.school.what.service.OAuthLoginService;
 import kakao.school.what.util.KakaoLoginParams;
 import kakao.school.what.util.NaverLoginParams;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,13 +28,18 @@ public class OauthController {
 
     @GetMapping("/naver")
     public ResponseEntity<String> loginNaver(@RequestParam("code") String code,
-                                                        @RequestParam("code") String state) {
+                                                        @RequestParam("code") String state,
+                                             HttpServletResponse response) {
         NaverLoginParams params = new NaverLoginParams();
         params.setCode(code);
         params.setState(state);
-        oAuthLoginService.login(params);
+        OAuthInfoResponse res = oAuthLoginService.login(params);
 
         String externalUrl = "http://localhost:3000/map";
+
+        Cookie cookie = new Cookie("email", res.getEmail());
+        cookie.setMaxAge(60 * 60);
+        response.addCookie(cookie);
 
         return ResponseEntity.status(HttpStatus.FOUND)
                 .header("Location", externalUrl)
