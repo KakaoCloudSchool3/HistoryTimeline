c29d1497f5d024a2757cf04a03cc03f2c87c44fc
lee <dlcksgur0919@naver.com>
2024-03-27 11:59:16 +0900

    :sparkles: content 연결

--- src/main/java/kakao/school/what/repository/ContentRepository.java
+++ src/main/java/kakao/school/what/repository/ContentRepository.java
@@ -3,10 +3,12 @@ package kakao.school.what.repository;
 import kakao.school.what.domain.Content;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ContentRepository extends JpaRepository<Content, Long> {
     // JpaRepository에서 제공하는 findById 메서드를 사용합니다.
     Optional<Content> findByTitle(String title);
 
+    List<Content> findByCountryIdAndTag(Long countryId, Integer tag);
 }
